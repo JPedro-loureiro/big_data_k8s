fcd5d3d0ba669c2772790b77bb6568de7015143e
JPedro-loureiro <joao_loureiro@outlook.com>
2021-11-26 19:13:45 -0300

    testing ingress path

--- terraform/argocd/argocd.tf
+++ terraform/argocd/argocd.tf
@@ -109,6 +109,57 @@ resource "kubernetes_manifest" "app_test_application" {
   }
 }
 
+resource "kubernetes_manifest" "ingress_test_application" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+
+    metadata = {
+      name = "ingress_test"
+      namespace = "cicd"
+    }
+
+    spec = {
+      project = "big-data-on-k8s"
+
+      source = {
+        repoURL = "https://github.com/JPedro-loureiro/big_data_k8s"
+        targetRevision = "HEAD"
+        path = "ingress-test"
+      }
+
+      destination = {
+        server = "https://kubernetes.default.svc"
+        namespace = "ingress-test"
+      }
+
+      syncPolicy = {
+        automated = {
+          prune = true
+          selfHeal = true
+          allowEmpty = false
+        }
+
+        syncOptions = [
+          "Validate=false",
+          "CreateNamespace=true",
+          "PrunePropagationPolicy=foreground",
+          "PruneLast=true"
+        ]
+
+        retry = {
+          limit = 3
+          backoff = {
+            duration = "5s"
+            factor = 2
+            maxDuration = "1m"
+          }
+        }
+      }
+    }
+  }
+}
+
 # Strimzi Operator
 resource "kubernetes_manifest" "strimzi_operator" {
   manifest = {
