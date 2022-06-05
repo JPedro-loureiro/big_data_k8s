2b44c8955c2fd40596dc12cca5739aca9dd13b8a
JPedro-loureiro <joao_loureiro@outlook.com>
2022-06-05 08:47:20 -0300

    deploy hive metastore

--- apps/data_exploration/hive-metastore/scripts/entrypoint.sh
+++ apps/data_exploration/hive-metastore/scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 export HADOOP_HOME=/opt/hadoop-3.2.0
 export HADOOP_CLASSPATH=${HADOOP_HOME}/share/hadoop/tools/lib/aws-java-sdk-bundle-1.11.375.jar:${HADOOP_HOME}/share/hadoop/tools/lib/hadoop-aws-3.2.0.jar
 export JAVA_HOME=/usr/local/openjdk-8
-export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}.data-exploration.svc.cluster.local
+export METASTORE_DB_HOSTNAME=${METASTORE_DB_HOSTNAME:-localhost}.metastore.svc.cluster.local
 
 echo "Waiting for database on ${METASTORE_DB_HOSTNAME} to launch on 3306 ..."
 
