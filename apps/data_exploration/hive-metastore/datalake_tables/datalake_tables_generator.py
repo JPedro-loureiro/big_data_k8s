d73e173bcb7874052abe920b8b1a749521a014df
JPedro-loureiro <joao_loureiro@outlook.com>
2022-06-22 08:01:18 -0300

    bug fix deploy datalake tables generator

--- apps/data_exploration/hive-metastore/datalake_tables/datalake_tables_generator.py
+++ apps/data_exploration/hive-metastore/datalake_tables/datalake_tables_generator.py
@@ -1,13 +1,15 @@
 import os
 from trino.dbapi import connect
 
+# while True:
+#     continue
 
 TRINO_HOST = "trino.data-exploration.svc.cluster.local"
 TRINO_PORT = "8080"
 TRINO_USERNAME = "trino"
 TRINO_CATALOG = "minio"
 
-current_path = f"{os.path.dirname(__file__)}/sql_ddl/"
+current_path = f"{os.getcwd()}/sql_ddl/"
 for schema in os.listdir(current_path):
 
     conn = connect(
@@ -22,7 +24,7 @@ for schema in os.listdir(current_path):
 
     for sql_file_path in os.listdir(f"{current_path}/{schema}/"):
 
-        with open(f"{current_path}/{sql_file_path}", "r") as sql_file:
+        with open(f"{current_path}/{schema}/{sql_file_path}", "r") as sql_file:
             print(f"executing {sql_file_path}")
             sql = sql_file.read()
 
